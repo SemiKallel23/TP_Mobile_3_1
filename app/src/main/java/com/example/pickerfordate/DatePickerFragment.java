4b8b47d7e29467fb1b646348d0ad7ae4ced6e838
PROG2-P4 <PROG2-P4-prog2@enetcom.usf.tn>
2024-10-21 16:45:28 +0100

    first commit

---
+++ app/src/main/java/com/example/pickerfordate/DatePickerFragment.java
@@ -0,0 +1,9 @@
+package com.example.pickerfordate;
+
+import android.app.DatePickerDialog;
+
+import androidx.fragment.app.DialogFragment;
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+}
